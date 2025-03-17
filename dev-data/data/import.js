c073178e2ccddd63d77ce93d279481f37d2494c5
Nwosu Paul <nwosupaulchinagorom@gmail.com>
2025-03-17 11:33:36 +0100

    imported data to database

--- dev-data/data/import.js
+++ dev-data/data/import.js
@@ -4,37 +4,36 @@ const dotenv = require("dotenv");
 const Tour = require("../../models/toursModel");
 const User = require("./../../models/userModel");
 const Review = require("./../../models/reviewModel");
+
 dotenv.config({ path: "./config.env" });
+
 const DB = process.env.DATABASE.replace(
   "<db_password>",
   process.env.DATABASE_PASSWORD
 );
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then((con) => {
-    // console.log("DB connection successful!");
-  });
+
+mongoose.connect(DB).then((con) => {
+  console.log("DB connection successful!");
+});
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
 const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
 const reviews = JSON.parse(
   fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
 );
+
 const importData = async () => {
   try {
-    await Tour.create(tours);
+    // await Tour.create(tours);
     await User.create(users, { validateBeforeSave: false });
     await Review.create(reviews, { validateBeforeSave: false });
-    // console.log("Data successfully loaded to the database");
+    console.log("Data successfully loaded to the database");
     process.exit();
   } catch (err) {
-    // console.log(err);
+    console.log(err);
   }
 };
+
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
@@ -43,9 +42,10 @@ const deleteData = async () => {
     console.log("Data successfully deleted");
     process.exit();
   } catch (err) {
-    // console.log(err);
+    console.log(err);
   }
 };
+
 if (process.argv[2] === "--import") {
   importData();
 } else if (process.argv[2] === "--delete") {
